64cb9ccdf03cf5c5ffd44e862b845fbf2fb01b48
Colin Yang <502470184@qq.com>
2020-04-22 18:06:11 +1000

    Update

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 let http = require("http");
 
 const PORT = 80;
+const RESPONSE = `\
+<p>
+  本地址包含be/je的mc服<br/>
+  <input value="mc.colintree.cn"></input>
+</p>
+<p>
+  并有je服网页地图
+  <ol>
+  <li>
+    <a href="http://mc.colintree.cn:8123" target="_blank">生存分服</a>
+    <small>（<a href="http://bot.colintree.cn:8123" target="_blank">海外分流</a>）</small>
+  </li>
+  <li>
+    <a href="http://mc.colintree.cn:8124" target="_blank">创造分服</a>
+    <small>（<a href="http://bot.colintree.cn:8124" target="_blank">海外分流</a>）</small>
+  </li>
+  </ol>
+</p>`;
 
 start();
 
 function start() {
   let server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
-    response.write("Welcome to bot.colintree.cn");
-    response.write("<ol>");
-    response.write("<li><a href=\"http://mc.colintree.cn:8123\">生存服网页地图</a></li>");
-    response.write("<li><a href=\"http://mc.colintree.cn:8124\">创造服网页地图</a></li>");
-    response.write("</ol>");
-    response.end();
+    response.end(RESPONSE);
   });
   console.log("listening port at: " + PORT);
   server.listen(PORT);
